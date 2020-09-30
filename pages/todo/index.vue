fb94ad5272de6deb21ce1657ab6564c2d6adc5eb
Renan Reis <renan.reis@esig.com.br>
2020-09-30 00:06:33 -0300

    Component Todo.

--- pages/todo/index.vue
+++ pages/todo/index.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="container container-nuxt">
     <div class="title">
-      <h1>Todos</h1>
+      <h1 class="text-center">Todos</h1>
+    </div>
+
+    <div class="container-fluid mt-5">
+      <div class="card">
+        <div class="card-header">
+          Start typing
+        </div>
+        <div class="card-body">
+          <div class="row p-1" v-for="(item, index) in 4" :key="index">
+            <div class="col-10">
+              <li>First</li>
+            </div>
+
+            <div class="col-2 text-right">
+              <button class="btn btn-danger btn-sm">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -18,4 +42,5 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
